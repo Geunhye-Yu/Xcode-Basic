85e91410f532d4a4a3a4a9dc5d8a0bac414a7d93
Geunhye-Yu <yoo4003@gmail.com>
2022-12-06 12:19:34 +0100

    IBAction and IBobject

--- WarCardGame/WarCardGame/ViewController.swift
+++ WarCardGame/WarCardGame/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LeftCardImageView: UIImageView!
+    
+    @IBOutlet weak var RightCardImageView: UIImageView!
+    
+    @IBOutlet weak var LeftScoreLabel: UILabel!
+    
+    @IBOutlet weak var RightScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func DealButton(_ sender: Any) {
+        
+        print("Deal Tapped")
+    }
+    
 }
 
