0032467c31b491fb1842fa9b70ca5ca04359a7f5
Geunhye-Yu <yoo4003@gmail.com>
2023-02-17 13:19:43 +0100

    Project2 implemented : when three flags are presented, the user pick the one which matches the name of a country in the title area. the game is limited with 10 rounds

---
+++ Swift_Project/Project2/Project2/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Geunhye Yu on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var roundCount = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+        
+        
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil){
+
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased() + " / Score: \(score)"
+ 
+        
+    }
+    
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+        var title: String
+        roundCount += 1
+        
+        if roundCount <= 10{
+            if sender.tag == correctAnswer {
+                title = "Win"
+                score += 1
+            } else{
+                title = "Wrong! That's the flag of \(countries[sender.tag])"
+                score -= 1
+            }
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler:askQuestion))
+            present(ac, animated: true)
+        }else{
+            title = "End"
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Game Ended", style: .destructive))
+            present(ac, animated: true)
+        }
+
+    }
+
+}
+
