b8789d01dc76913bf097eccff092e7c2377453a9
Geunhye-Yu <yoo4003@gmail.com>
2023-03-10 15:17:37 +0100

    Project4 revised - KVO added

--- Swift_Project/Project4/Project4/ViewController.swift
+++ Swift_Project/Project4/Project4/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressview: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -23,7 +24,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // Do any additional setup after loading the view.
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style:.plain, target: self, action: #selector(openTapped))
-
+        
+        // ** UIView subclasses: UIToolbar and UIProgressView
+        //For Progress bar
+        progressview = UIProgressView(progressViewStyle: .default)
+        progressview.sizeToFit()
+        //can't just add UIView subclasses to a UIToolbar, or to the rightBarButtonItem property but a collection of UIBarButtonItem objects
+        let progressButton = UIBarButtonItem(customView: progressview)
+        
+        //For Toolbar
+        //the effect of the flexible space automatically taking up as much room as it can on the left
+        //it does not functioning like a button or action but just a spacer
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        //add observer that can be notified when the progress is changed
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+        
+    }
+    
+    // when we use addObserver, we need to difine observerValue and removeObserver
+    // this method is called an observed value has changed
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressview.progress = Float(webView.estimatedProgress)
+        }
     }
     
     @objc func openTapped(){
