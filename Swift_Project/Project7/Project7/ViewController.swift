8f8b977f5c5fc9dedd70e3bd62d3dca0d4e4d2f1
Geunhye-Yu <yoo4003@gmail.com>
2023-04-12 11:01:09 +0200

    Project7: Add an alert msg to show info & to filter the list with text which is given

--- Swift_Project/Project7/Project7/ViewController.swift
+++ Swift_Project/Project7/Project7/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allPetitions = [Petition]()
     var petitions = [Petition]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .plain, target: self, action: #selector(showInfo))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet.rectangle.fill"), style: .plain, target: self, action: #selector(promptSearchText))
+        
         
         let urlString: String
         if navigationController?.tabBarItem.tag == 0{
@@ -33,6 +39,7 @@ class ViewController: UITableViewController {
         }
         
         showError()
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,6 +65,7 @@ class ViewController: UITableViewController {
         
         if let jsonPetition = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetition.results
+            allPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -67,7 +75,55 @@ class ViewController: UITableViewController {
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
+    
+    @objc func showInfo(){
+        let ac = UIAlertController(title: "Info", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func promptSearchText(){
+        
+        //var keyword = "President Trump"
+        
+        let ac = UIAlertController(title: "Find", message: "Put the text you want to find", preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default){[weak self, weak ac] action in
+            guard let keyword = ac?.textFields?[0].text else{ return }
+            self?.filterText(keyword: keyword, filterStatus: true)
+        }
+        ac.addAction(submitAction)
+        
+        let backToAllAction = UIAlertAction(title: "Back to All", style: .cancel){[weak self] action in
+            self?.filterText(keyword: "", filterStatus: false)
+        }
+        ac.addAction(backToAllAction)
+        
+        present(ac, animated: true)
+        
+    }
      
+    func filterText(keyword: String, filterStatus: Bool){
+        
+        var filteredPetitions = [Petition]()
+        
+        if filterStatus{
+            for (_, item) in allPetitions.enumerated(){
+                if item.title.contains(keyword){
+                    filteredPetitions.append(item)
+                }
+            }
+            petitions = filteredPetitions
+
+            
+        } else {
+            petitions = allPetitions
+
+        }
+
+        tableView.reloadData()
+        
+    }
 
 }
 
